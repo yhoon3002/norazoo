8de2bf4853a48c066b43183b753014e5b218a421
0H00NI <yhoon3002@gmail.com>
2025-06-21 17:21:27 +0900

    모바일 safe-area height 수정

--- src/app/presenter/home/HomePresenter.tsx
+++ src/app/presenter/home/HomePresenter.tsx
@@ -29,13 +29,13 @@ export default function HomePresenter(props: HomePresenterProps) {
             {props.openModal && (
                 <>
                     <div className="w-screen h-screen flex flex-none justify-center items-center p-10 fixed z-50 inset-0 bg-black bg-opacity-40">
-                        <div className="w-full h-full min-w-[320px] flex flex-col bg-white rounded-2xl shadow-2xl p-8 divide-y-2 divide-solid">
+                        <div className="w-full h-full max-h-[100dvh] min-w-[320px] flex flex-col bg-white rounded-2xl shadow-2xl p-8 divide-y-2 divide-solid">
                             <ModalTitle
                                 currentGame={props.currentGame}
                                 handleCloseModal={props.handleCloseModal}
                             ></ModalTitle>
 
-                            <div className="h-full max-h-[100dvh] flex flex-col">
+                            <div className="h-full flex flex-col">
                                 {props.currentGame?.name === "Jaeum Moeum" && (
                                     <JaeummoeumContainer
                                         handleGameLoad={() =>
